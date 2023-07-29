f5b052cb01322b01ebee871118bac6b5b350e8ed
DmytroKhmaruk <dima.khmaruk7@gmail.com>
2023-07-29 09:38:41 +0200

    done hw06

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import Container from './Container';
+import Contacts from './Contacts';
+import ContactForm from './ContactForm';
+import Filter from './Filter';
+import { useDispatch, useSelector } from 'react-redux';
+import { setContact, setFilter, setRemoveContact } from 'redux/phonebookSlice';
+
+export default function App() {
+  const contacts = useSelector(state => state.contacts.contacts);
+  const filter = useSelector(state => state.contacts.filter);
+  const dispatch = useDispatch();
+
+  const handleChangeFilter = evt => {
+    dispatch(setFilter(evt.target.value));
+  };
+
+  const filterContact = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
   );
-};
+
+  const formAddContact = contactData => {
+    const isExistingContact = contacts.some(contact => contact.name === contactData.name);
+
+    if (isExistingContact) {
+      alert('This contact already exists!');
+      return;
+    }
+
+    dispatch(setContact(contactData));
+  };
+
+  return (
+    <Container>
+      <h1>Phonebook</h1>
+
+      <ContactForm
+        formAddContact={formAddContact}
+        contactsArr={contacts} />
+      
+      <h2>Contacts</h2>
+      
+      <Filter value={filter} handleChangeFilter={handleChangeFilter}/>
+      
+      <Contacts contact={filterContact} removeContact={contactId => dispatch(setRemoveContact(contactId))}/>
+
+      </Container>       
+  );
+}
---
+++ src/components/Delete/Delete.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import { MdDeleteForever } from 'react-icons/md';
+import DeleteStyles from './DeleteStyles'
+
+
+
+const Delete = ({id, name, number, onRemoveContact})=>{
+    return(
+        <DeleteStyles key={id}>
+            <p>
+              {name}: {number}
+            </p>
+        <button type='button' onClick={() => onRemoveContact(id)}>
+          <MdDeleteForever/>
+        </button>
+          </DeleteStyles>
+    )
+}
+
+Delete.propTypes={
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    number: PropTypes.string.isRequired,
+    onRemoveContact: PropTypes.func.isRequired
+}
+
+export default Delete
